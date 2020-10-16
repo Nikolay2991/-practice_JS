80744791bbf1fed738dbc0634c74ccf7e99b457d
Nikolay <jump9207@mail.ru>
2020-10-16 14:04:57 +0300

    day 4

--- js/main.js
+++ js/main.js
@@ -195,8 +195,58 @@ function writeYourGenres() {
     }
 }
 
-writeYourGenres();
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log("Test");
+    }
+};
 
+options.makeTest();
+
+const {border, bg} = options.colors;
+console.log(bg);
+
+// console.log(Object.keys(options));
+
+// console.log(Object.keys(options).length);
+
+// delete options.name;
+
+// console.log(options);
+let counter = 0;
+for (let key in options) {
+    if (typeof(options[key]) === 'object') {
+        for (let i in options[key]) {
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+        }
+    } else {
+        console.log(`Свойство ${key} имеет значение ${options[key]}`);
+    }
+    counter++;
+}
+
+console.log(counter);
+
+// writeYourGenres();
+
+
+// function learnJS(lang, callback) {
+//     console.log(`Я учу: ${lang}`);
+//     callback();
+// }
+
+// function done() {
+//     console.log('Я прошел этот урок!');
+// }
+
+// learnJS('JavaScript', done);
 
 // function showFirstMessage(text) {
 //     console.log(text);
